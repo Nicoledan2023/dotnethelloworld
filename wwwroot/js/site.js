f567a726cdee8f136e6319c4152f6b487b6fe66e
Dan <1850484@cegep-heritage.qc.ca>
2023-04-05 15:25:49 -0400

    add changes

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+window.addEventListener('load', function () {
+    if (document.title === 'Contact') {
+        console.log('Contact page loaded.');
+    }
+  });
